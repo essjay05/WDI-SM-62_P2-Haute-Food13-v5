97971595a6766293bfa45fe4b9c96163619117a2
Joy <stepjoyous@gmail.com>
2019-01-15 18:24:08 -0800

    Re-added email uniqueness validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     #Invoke bcrypt to require AND hash passwords
     has_secure_password
+    #If User is deleted, its posts are deleted as well
     has_many :posts, dependent: :destroy
+    #Checks if email already exists in database so no duplicate emails can be created... need to figure out how to send error
+    validates_uniqueness_of :email
 end
