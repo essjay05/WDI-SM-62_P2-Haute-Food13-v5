6cad7441fe4fb00b2c495130552d12b648cb66f8
Joy <stepjoyous@gmail.com>
2019-01-17 10:29:09 -0800

    Setting up Cloudinary and Pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,9 +1,9 @@
 
 require 'pusher'
 
-Pusher.app_id = '690106' #PUSHER_APP_ID
-Pusher.key = '063e9b007a3a998ef7f1' #PUSHER_APP_KEY
-Pusher.secret = 'e06bba137907cfe0ca4b' #PUSHER_APP_SECRET
-Pusher.cluster = 'mt1' #PUSHER_APP_CLUSTER
+Pusher.app_id = 'PUSHER_APP_ID'
+Pusher.key = 'PUSHER_APP_KEY'
+Pusher.secret = 'PUSHER_APP_SECRET'
+Pusher.cluster = 'PUSHER_APP_CLUSTER'
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
